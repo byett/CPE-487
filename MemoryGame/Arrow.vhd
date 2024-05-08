20e27afbc2c30d2cf5446b9b69e654f36aaa8ed2
John Li <122575719+JohnMFB@users.noreply.github.com>
2024-05-07 20:23:35 -0400

    Update Arrow.vhd
    Small fixes but overall still not working with rgb

--- MemoryGame/Arrow.vhd
+++ MemoryGame/Arrow.vhd
@@ -2,34 +2,37 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 ENTITY Arrow IS
-	PORT (
-        v_sync      : IN  STD_LOGIC;
-        pixel_row   : IN  STD_LOGIC_VECTOR(10 DOWNTO 0);
-        pixel_col   : IN  STD_LOGIC_VECTOR(10 DOWNTO 0);
-        red         : OUT STD_LOGIC;
-        green       : OUT STD_LOGIC; --DONE IN vga_top
-        blue        : OUT STD_LOGIC;
+    PORT (
+        v_sync          : IN  STD_LOGIC;
+        pixel_row       : IN  STD_LOGIC_VECTOR(10 DOWNTO 0);
+        pixel_col       : IN  STD_LOGIC_VECTOR(10 DOWNTO 0);
+        red             : OUT STD_LOGIC;
+        green           : OUT STD_LOGIC;
+        blue            : OUT STD_LOGIC;
         arrow_direction : IN INTEGER range 1 to 4;
-        color_chosen : IN INTEGER range 1 to 3
-	);
+        color_chosen    : IN INTEGER range 1 to 3
+    );
 END Arrow;
+
 ARCHITECTURE Behavioral OF Arrow IS
-	CONSTANT size  : INTEGER := 8;
-	SIGNAL ball_on : STD_LOGIC; -- indicates whether ball is over current pixel position
-	-- current ball position - intitialized to center of screen
-	SIGNAL ball_x  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
-	SIGNAL ball_y  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
-	-- current ball motion - initialized to +4 pixels/frame
-	SIGNAL ball_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000000";
-	
+    CONSTANT size  : INTEGER := 8;
+    SIGNAL ball_on : STD_LOGIC :='0'; -- indicates whether ball is over current pixel position
+    -- current ball position - initialized to center of screen
+    SIGNAL ball_x  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
+    SIGNAL ball_y  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
+    
 	BEGIN 	-- Behav start
 	
-	arrow_draw : PROCESS (ball_x, ball_y, pixel_row, pixel_col, arrow_direction, color_chosen) IS
+    --red <= '1';
+    --green <= NOT ball_on;
+    --blue <= NOT ball_on;
+	
+	arrow_draw : PROCESS (ball_x, ball_y, pixel_row, pixel_col, arrow_direction, color_chosen, ball_on) IS
 	BEGIN
-	--	red <= '1';
-   -- green <= NOT ball_on;
-   -- blue <= NOT ball_on;
+	ball_on <= '0';
+	
 	-- process to draw ball current pixel address is covered by ball position
    IF (arrow_direction = 1) THEN
             IF (pixel_col >= ball_x AND
@@ -100,9 +103,13 @@ ARCHITECTURE Behavioral OF Arrow IS
 	            ball_on <= '0';
 	        END IF;
     ELSE
+    ball_on <= '0';
     -- No arrow shows
    
     END IF;
+   -- red <= '1';
+   -- green <= NOT ball_on;
+   -- blue <= NOT ball_on;
     END PROCESS;
 
 END Behavioral;
